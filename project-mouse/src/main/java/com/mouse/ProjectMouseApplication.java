ba4d0fe8cbe46d8f63bba9d4a8d8a2a171440ffb
huayadlly <huayadlly@gmail.com>
2017-05-16 22:16:13 +0800

    搭建module项目；

---
+++ project-mouse/src/main/java/com/mouse/ProjectMouseApplication.java
@@ -0,0 +1,12 @@
+package com.mouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMouseApplication.class, args);
+	}
+}
