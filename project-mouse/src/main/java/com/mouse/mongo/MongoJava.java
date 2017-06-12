d3d51628ef18a1d4d8322f825e6cef1c6d563240
huayadlly <huayadlly@gmail.com>
2017-06-13 00:03:00 +0800

    更新文档；

--- project-mouse/src/main/java/com/mouse/mongo/MongoJava.java
+++ project-mouse/src/main/java/com/mouse/mongo/MongoJava.java
@@ -2,8 +2,10 @@ package com.mouse.mongo;
 
 import com.google.common.collect.Lists;
 import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.Document;
 
@@ -47,8 +49,36 @@ public class MongoJava {
         }
     }
 
+    public void updateDocument() {
+        try {
+            //连接数据库
+            MongoClient client = new MongoClient("localhost", 27017);
+            //选择数据库
+            MongoDatabase mydb = client.getDatabase("mydb");
+            //选择集合
+            MongoCollection<Document> test = mydb.getCollection("test");
+            log.info("选择集合 成功");
+
+            //更新文档  更新文档的likes值
+            test.updateMany(Filters.eq("likes", 120), new Document("$set", new Document("likes", 66)));
+            log.info("更新集合 成功");
+
+            //查看检索结果
+            FindIterable<Document> documents = test.find();
+            for (Document document : documents) {
+                System.out.println("文档：" + document);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         MongoJava mongoJava = new MongoJava();
-        mongoJava.mongoJdbc();
+        //添加文档
+//        mongoJava.mongoJdbc();
+
+        //更新文档
+        mongoJava.updateDocument();
     }
 }
