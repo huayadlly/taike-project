56ad85f4a0070a1d589eb1995a4101ea5cfdc3bd
huayadlly <huayadlly@gmail.com>
2017-06-13 00:28:43 +0800

    删除文档；

--- project-mouse/src/main/java/com/mouse/mongo/MongoJava.java
+++ project-mouse/src/main/java/com/mouse/mongo/MongoJava.java
@@ -7,7 +7,10 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
 import lombok.extern.slf4j.Slf4j;
+import org.bson.BsonDocument;
 import org.bson.Document;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.conversions.Bson;
 
 import java.util.List;
 
@@ -49,6 +52,7 @@ public class MongoJava {
         }
     }
 
+    //更新文档
     public void updateDocument() {
         try {
             //连接数据库
@@ -73,12 +77,39 @@ public class MongoJava {
         }
     }
 
+
+    //删除文档
+    public void deleteDocument() {
+        try {
+            MongoClient client = new MongoClient("localhost", 27017);
+            MongoDatabase myCollection = client.getDatabase("myCollection");
+            MongoCollection<Document> myCollection1 = myCollection.getCollection("myCollection");
+            log.info("选择集合 成功");
+
+            //删除符合条件的文档
+            myCollection1.deleteOne(Filters.eq("title", "demo"));
+            log.info("删除集合 成功");
+
+            //查看删除结果
+            FindIterable<Document> documents = myCollection1.find();
+            for (Document document : documents) {
+                System.out.println("删除结果：" + document);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         MongoJava mongoJava = new MongoJava();
         //添加文档
 //        mongoJava.mongoJdbc();
 
         //更新文档
-        mongoJava.updateDocument();
+//        mongoJava.updateDocument();
+
+        //删除文档
+        mongoJava.deleteDocument();
     }
 }
