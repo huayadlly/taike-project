f3dcc46444240e0e72bf0f36862c7557b32dbb4d
huayadlly <huayadlly@gmail.com>
2017-06-12 22:40:40 +0800

    向mongodb中添加文档；

--- project-mouse/src/main/java/com/mouse/mongo/MongoJava.java
+++ project-mouse/src/main/java/com/mouse/mongo/MongoJava.java
@@ -4,7 +4,7 @@ import com.google.common.collect.Lists;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
 import org.bson.Document;
 
 import java.util.List;
@@ -12,45 +12,43 @@ import java.util.List;
 /**
  * Created by huayadlly on 2017/6/11.
  */
-@Log4j
+@Slf4j
 public class MongoJava {
 
-    private static final String HOST = "localhost";
-    private static final Integer PORT = 27027;
-    private static MongoClient client;
-
-    public MongoJava() {
+    public void mongoJdbc() {
         try {
-            client = new MongoClient(HOST, PORT);
-            log.info("Connection to MongoDB Database Success!");
+            //连接数据库
+            MongoClient mongoClient = new MongoClient("localhost", 27017);
+            MongoDatabase mydb = mongoClient.getDatabase("mydb");
+            log.info("Connection to MongoDB Successful");
+
+            //创建集合
+//            mydb.createCollection("test");
+//            log.info("Create Collection Successful");
+
+            //选择集合
+            MongoCollection<Document> collection = mydb.getCollection("test");
+            log.info("选择集合 成功");
+
+            //向集合中添加文档
+            Document document = new Document();
+            document.append("title", "MongoDB");
+            document.append("description", "MongoDB is no SQL database");
+            document.append("likes", 87);
+            document.append("tags", Lists.newArrayList("Java", "PHP", "C#"));
+            List<Document> list = Lists.newArrayList(document);
+
+            //向集合中插入文档
+            collection.insertMany(list);
+            log.info("向集合中插入文档 成功");
+
         } catch (Exception e) {
-            log.error("Fail to Connection MongoDB!");
             e.printStackTrace();
         }
     }
 
-    public void mongoJdbc() {
-        //连接数据库
-        MongoDatabase database = client.getDatabase("myDb");
-        database.createCollection("myCollection");
-        log.info("Create Collection Successful !");
-
-//        MongoCollection<Document> myCollection = database.getCollection("myCollection");
-//
-//        //向mongodb数据库中插入文档
-//        Document document = new Document("title", "mongo_document");
-//        document.append("description", "MongoDB Document");
-//        document.append("likes", 100);
-//        document.append("by", "Guava");
-//        document.append("project", "Java");
-//
-//        List<Document> list = Lists.newArrayList(document);
-//
-//        myCollection.insertMany(list);
-//        log.info("数据插入成功！");
-    }
-
     public static void main(String[] args) {
-        new MongoJava().mongoJdbc();
+        MongoJava mongoJava = new MongoJava();
+        mongoJava.mongoJdbc();
     }
 }
