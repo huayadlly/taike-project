0d0072dfe1806acb998fddfa2171c733aabe5f89
huayadlly <huayadlly@gmail.com>
2017-06-13 23:55:10 +0800

    jpa mongodb;

---
+++ project-mouse/src/main/java/com/mouse/mongo/MongoController.java
@@ -0,0 +1,13 @@
+package com.mouse.mongo;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by huayadlly on 2017/6/13.
+ */
+@Controller
+public class MongoController {
+
+    //向mongodb中添加数据
+
+}
