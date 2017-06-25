26db0927bf4844573c95fd41f7473bc2dfbbe4e7
huayadlly <huayadlly@gmail.com>
2017-06-25 14:50:15 +0800

    添加data mongo依赖

--- project-mouse/src/main/java/com/mouse/mongo/MongoController.java
+++ project-mouse/src/main/java/com/mouse/mongo/MongoController.java
@@ -1,11 +1,21 @@
 package com.mouse.mongo;
 
 import com.google.common.collect.Maps;
+import com.mouse.entity.Book;
+import com.mouse.folder.PageUtils;
+import com.mouse.jpa.BookJpaRepository;
+import com.mouse.mongo.jpa.BookMongoDbRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -14,6 +24,11 @@ import java.util.Map;
 @Controller
 public class MongoController {
 
+    @Autowired
+    private BookJpaRepository bookJpaRepository;
+    @Autowired
+    private BookMongoDbRepository bookMongoDbRepository;
+
     //向mongodb中添加数据
     @RequestMapping(value = "/mongodb/add/book", method = RequestMethod.POST)
     @ResponseBody
@@ -21,6 +36,13 @@ public class MongoController {
         Map<String, String> msg = Maps.newHashMap();
         try {
 
+            //分页查询
+            Pageable pageable = new PageRequest(2,10, Sort.Direction.ASC);
+            Page<Book> bookByPage = bookJpaRepository.findAll(pageable);
+            List<Book> bookList = bookByPage.getContent();
+
+            //向mongo数据库中存
+            bookMongoDbRepository.save(bookList);
 
             msg.put("SUCCESS", "YES");
         } catch (Exception e) {
--- project-mouse/src/main/java/com/mouse/mongo/jpa/BookMongoDbRepository.java
+++ project-mouse/src/main/java/com/mouse/mongo/jpa/BookMongoDbRepository.java
@@ -2,11 +2,12 @@ package com.mouse.mongo.jpa;
 
 import com.mouse.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by huayadlly on 2017/6/24.
  */
+@Repository
 public interface BookMongoDbRepository extends JpaRepository<Book, Integer> {
 
-    Book findAllByBookId();
 }
